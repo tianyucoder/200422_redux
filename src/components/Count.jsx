495faa979a575227235f281a35f1bdafadcc4503
tianyu5177 <zhangtianyu5177@sina.com>
2020-09-05 14:59:46 +0800

    更新

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import store from '../redux/store'
 
 export default class Count extends Component {
 
@@ -9,40 +10,33 @@ export default class Count extends Component {
 	increment = ()=>{
 		//1.获取用户选择的数字
 		const {selectedNumber} = this.refs
-		//2.获取原状态
-		// const {sum} = this.state
-		//3.执行加
-		// this.setState({sum:sum + selectedNumber.value*1})
+		//通知redux + selectedNumber.value
+		store.dispatch({type:'increment',data:selectedNumber.value*1})
 	}
 
 	decrement = ()=>{
 		//1.获取用户选择的数字
 		const {selectedNumber} = this.refs
-		//2.获取原状态
-		// const {sum} = this.state
-		//3.执行加
-		// this.setState({sum:sum - selectedNumber.value*1})
+		//通知redux - selectedNumber.value
+		store.dispatch({type:'decrement',data:selectedNumber.value*1})
 	}
 
 	incrementIfodd = ()=>{
 			//1.获取用户选择的数字
 			const {selectedNumber} = this.refs
 			//2.获取原状态
-			// const {sum} = this.state
-			// if(sum % 2 !== 0){
-			// 	//3.执行加
-			// 	this.setState({sum:sum + selectedNumber.value*1})
-			// }
+			const sum = store.getState()
+			if(sum % 2 !== 0){
+				//3.执行加
+				store.dispatch({type:'increment',data:selectedNumber.value*1})
+			}
 	}
 	
 	incrementAsync = ()=>{
 		//1.获取用户选择的数字
 		const {selectedNumber} = this.refs
-		//2.获取原状态
-		// const {sum} = this.state
 		setTimeout(()=>{
-			//3.执行加
-			// this.setState({sum:sum + selectedNumber.value*1})
+			store.dispatch({type:'increment',data:selectedNumber.value*1})
 		},500)
 	}
 
@@ -50,7 +44,7 @@ export default class Count extends Component {
 		// const {sum} = this.state
 		return (
 			<div>
-				<h1>当前求和为？？？</h1>
+				<h1>当前求和为{store.getState()}</h1>
 				<select ref="selectedNumber">
 					<option value="1">1</option>
 					<option value="2">2</option>
