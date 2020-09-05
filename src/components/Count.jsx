e9a33d5c419889edabc647ef980cc0b316623ce8
tianyu5177 <zhangtianyu5177@sina.com>
2020-09-05 11:59:52 +0800

    更新

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -2,55 +2,55 @@ import React, { Component } from 'react'
 
 export default class Count extends Component {
 
-	state = {
+	/* state = {
 		sum:0
-	}
+	} */
 
 	increment = ()=>{
 		//1.获取用户选择的数字
 		const {selectedNumber} = this.refs
 		//2.获取原状态
-		const {sum} = this.state
+		// const {sum} = this.state
 		//3.执行加
-		this.setState({sum:sum + selectedNumber.value*1})
+		// this.setState({sum:sum + selectedNumber.value*1})
 	}
 
 	decrement = ()=>{
 		//1.获取用户选择的数字
 		const {selectedNumber} = this.refs
 		//2.获取原状态
-		const {sum} = this.state
+		// const {sum} = this.state
 		//3.执行加
-		this.setState({sum:sum - selectedNumber.value*1})
+		// this.setState({sum:sum - selectedNumber.value*1})
 	}
 
 	incrementIfodd = ()=>{
 			//1.获取用户选择的数字
 			const {selectedNumber} = this.refs
 			//2.获取原状态
-			const {sum} = this.state
-			if(sum % 2 !== 0){
-				//3.执行加
-				this.setState({sum:sum + selectedNumber.value*1})
-			}
+			// const {sum} = this.state
+			// if(sum % 2 !== 0){
+			// 	//3.执行加
+			// 	this.setState({sum:sum + selectedNumber.value*1})
+			// }
 	}
 	
 	incrementAsync = ()=>{
 		//1.获取用户选择的数字
 		const {selectedNumber} = this.refs
 		//2.获取原状态
-		const {sum} = this.state
+		// const {sum} = this.state
 		setTimeout(()=>{
 			//3.执行加
-			this.setState({sum:sum + selectedNumber.value*1})
+			// this.setState({sum:sum + selectedNumber.value*1})
 		},500)
 	}
 
 	render() {
-		const {sum} = this.state
+		// const {sum} = this.state
 		return (
 			<div>
-				<h1>当前求和为：{sum}</h1>
+				<h1>当前求和为？？？</h1>
 				<select ref="selectedNumber">
 					<option value="1">1</option>
 					<option value="2">2</option>
