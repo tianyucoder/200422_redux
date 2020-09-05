65a0f97637f5687618097c7e98c907c93217f050
tianyu5177 <zhangtianyu5177@sina.com>
2020-09-05 16:35:42 +0800

    更新

---
+++ 04_src_redux_异步action/redux/count_action.js
@@ -0,0 +1,15 @@
+import {INCREMENT,DECREMENT} from './constant'
+//该文件是用于创建action对象的
+export const incrementAction  = number => ({type:INCREMENT,data:number})
+//异步action
+//异步action中一般都会调用一个对应的同步action
+export const incrementWaitAction  = number => {
+	return (dispatch)=>{
+		setTimeout(()=>{
+			dispatch(incrementAction(number))
+		},500)
+	}
+}
+export const decrementAction  = number => ({type:DECREMENT,data:number})
+
+
