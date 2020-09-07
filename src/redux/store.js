f18d2dadaa26f272e61a449d2224f53b9108d262
tianyu5177 <zhangtianyu5177@sina.com>
2020-09-07 11:59:09 +0800

    更新

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,8 @@ import {createStore,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 //引入汇总之后的reducer
 import reducer from './reducers'
+//引入composeWithDevTools，用于让开发者工具找到store
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 //创建并暴露store对象
-export default createStore(reducer,applyMiddleware(thunk))
+export default createStore(reducer,composeWithDevTools(applyMiddleware(thunk)))
